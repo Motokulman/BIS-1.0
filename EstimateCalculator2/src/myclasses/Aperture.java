4f4a9944b6a85146444385b7af203dc8b3476c4e
Motokulman <2594501@mail.ru>
2018-01-24 07:39:09 +0300

    Add files via upload

---
+++ EstimateCalculator2/src/myclasses/Aperture.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package myclasses;
+
+/**
+ *
+ * @author I
+ */
+public class Aperture extends RectangleAreaByWidthAndHeight {
+    
+    public Aperture(double W, double H) {
+        super(W, H);
+    }
+}
